86401a48a68645c9e69bb1ad3781b4145e02a54e
Kaz Ueno <kazun.ue@gmail.com>
2026-01-21 00:17:37 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: [:index]
-  root 'products#index'
+  resources :products, only: [:index]
+  root 'products#index'
 end
