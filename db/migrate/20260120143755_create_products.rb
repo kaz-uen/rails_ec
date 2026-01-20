995071cfef122ce7859f28a351dced68fe22b1eb
Kaz Ueno <kazun.ue@gmail.com>
2026-01-20 23:43:20 +0900

    マイグレーションファイル編集

--- db/migrate/20260120143755_create_products.rb
+++ db/migrate/20260120143755_create_products.rb
@@ -1,15 +1,17 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :sku
-      t.string :name
-      t.text :description
-      t.integer :price
+      t.string :sku, null: false
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price, null: false
       t.integer :original_price
-      t.integer :stock_quantity
-      t.boolean :is_on_sale
+      t.integer :stock_quantity, null: false
+      t.boolean :is_on_sale, null: false, default: false
 
       t.timestamps
     end
+
+    add_index :products, :sku, unique: true
   end
 end
