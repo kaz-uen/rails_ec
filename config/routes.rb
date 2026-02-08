48fb66be0dbcfb989d5be0a3d0ba430129c67ff5
Kaz Ueno <kazun.ue@gmail.com>
2026-02-09 08:02:25 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: %i[index show]
+  resources :products, only: %i[index show] do
+    member do
+      post 'add_to_cart'
+    end
+  end
   root 'products#index'
+
+  resource :cart, only: [:show] do
+    delete 'remove_item/:product_id', to: 'carts#destroy_item', as: :remove_item
+  end
 
   namespace :admin do
     resources :products, except: [:show]
