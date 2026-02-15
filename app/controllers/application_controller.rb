7164800ce859032a5ed4fdc30a1357bc0b3c586d
Kaz Ueno <kazun.ue@gmail.com>
2026-02-15 21:30:44 +0900

    add_product_to_cartとremove_from_cartをCartItemsControllerに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,23 +28,4 @@ class ApplicationController < ActionController::Base
   def cart_total
     current_cart.total
   end
-
-  def add_product_to_cart(product_id, quantity = 1)
-    product = Product.find(product_id)
-    cart_item = current_cart.cart_items.find_or_initialize_by(product_id: product.id)
-
-    if cart_item.new_record?
-      # 新規作成時は、そのまま数量を設定
-      cart_item.quantity = quantity
-    else
-      # 既存レコードの場合は、既存数量に追加
-      cart_item.quantity += quantity
-    end
-
-    cart_item.save!
-  end
-
-  def remove_from_cart(product_id)
-    current_cart.cart_items.where(product_id:).destroy_all
-  end
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,4 +14,25 @@ class CartItemsController < ApplicationController
     remove_from_cart(params[:product_id])
     redirect_to cart_path, notice: 'カートから削除しました。'
   end
+
+  private
+
+  def add_product_to_cart(product_id, quantity = 1)
+    product = Product.find(product_id)
+    cart_item = current_cart.cart_items.find_or_initialize_by(product_id: product.id)
+
+    if cart_item.new_record?
+      # 新規作成時は、そのまま数量を設定
+      cart_item.quantity = quantity
+    else
+      # 既存レコードの場合は、既存数量に追加
+      cart_item.quantity += quantity
+    end
+
+    cart_item.save!
+  end
+
+  def remove_from_cart(product_id)
+    current_cart.cart_items.where(product_id:).destroy_all
+  end
 end
