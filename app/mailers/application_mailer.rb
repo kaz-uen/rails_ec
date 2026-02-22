e569598958bcc5c37ddea0f16fdaaa0c32463b02
Kaz Ueno <kazun.ue@gmail.com>
2026-02-23 00:25:16 +0900

    メールの送信元アドレスを設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('MAILER_FROM', 'noreply@example.com')
+  default from: ENV.fetch('MAILER_FROM', 'postmaster@sandbox0bf65d00c19f473986582bf32e38923e.mailgun.org')
   layout 'mailer'
 end
