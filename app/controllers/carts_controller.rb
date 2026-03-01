fcd1315ea89fbfdaa5a78e9e366a21c49678e81d
Kaz Ueno <kazun.ue@gmail.com>
2026-03-02 07:27:53 +0900

    カート画面のプロモーションコード入力欄を有効化

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,23 @@ class CartsController < ApplicationController
   def show
     @cart_items = cart_items
     @cart_total = cart_total
+    @promotion_code = PromotionCode.find_by(code: session[:promotion_code]) if session[:promotion_code]
+  end
+
+  def apply_promotion_code
+    code = params[:promotion_code]
+    promotion_code = PromotionCode.available.find_by(code:)
+
+    if promotion_code
+      session[:promotion_code] = code
+      redirect_to cart_path, notice: 'プロモーションコードを適用しました。'
+    else
+      redirect_to cart_path, alert: '無効なプロモーションコードです。'
+    end
+  end
+
+  def remove_promotion_code
+    session[:promotion_code] = nil
+    redirect_to cart_path, notice: 'プロモーションコードを解除しました。'
   end
 end
