8138fa435bf5cf369232b05bdc20a6382ac0f265
Kaz Ueno <kazun.ue@gmail.com>
2026-03-02 07:20:40 +0900

    PromotionCodeモデル作成

---
+++ db/migrate/20260301080446_create_promotion_codes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreatePromotionCodes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :promotion_codes do |t|
+      t.string :code, null: false
+      t.integer :discount_amount, null: false
+      t.boolean :is_used, null: false, default: false
+
+      t.timestamps
+    end
+
+    add_index :promotion_codes, :code, unique: true
+  end
+end
